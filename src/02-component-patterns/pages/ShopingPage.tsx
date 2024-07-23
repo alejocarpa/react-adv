6bb0c0768a496d8588282035d5a26a3a45261fb8
Alejandro Cardenas <alejocarpa@hotmail.com>
2024-07-23 09:23:15 -0500

    Fin del patron

--- src/02-component-patterns/pages/ShopingPage.tsx
+++ src/02-component-patterns/pages/ShopingPage.tsx
@@ -2,70 +2,50 @@
 import { ProductButtons, ProductCard, ProductImage, ProductTitle } from "../components";
 import '../styles/custom-styles.css';
 import { products } from "../data/products";
-import { useShoppingCart } from "../hooks/useShoppingCart";
 
-
+const product = products[0];
 
 
 
 export const ShopingPage = () => {
 
-    const { shoppingCart, onProductCountChange } = useShoppingCart();
-
     return (
         <div>
             <h1>Shoping Store</h1>
             <hr />
-            <div style={{
-                display: 'flex',
-                flexDirection: 'row',
-                flexWrap: 'wrap'
-            }}>
-               
-                {
-                    products.map( product => (
 
-                        <ProductCard 
-                            key={ product.id }
-                            product={ product } 
-                            className="bg-dark text-white"
-                            onChange = { onProductCountChange }
-                            value= { shoppingCart[product.id]?.count || 0 }
-                        >
+
+
+            <ProductCard
+                key={product.id}
+                product={product}
+                className="bg-dark text-white"
+                initialValues = {{
+                    count: 4,
+                    maxCount: 10
+                }}
+            >
+                {
+                    ({ reset, count, increaseBy, isMaxCountReached }) => (
+                        <>
                             <ProductImage className="custom-image" />
                             <ProductTitle className="text-bold" />
                             <ProductButtons className="custom-buttons" />
-                        </ProductCard>
-                    ))
+
+                            <button onClick={ reset }>Reset</button>
+                            <button onClick={ () => increaseBy( -2 ) }>-2</button>
+                            {
+                                !isMaxCountReached
+                                &&
+                                <button onClick={ () => increaseBy( +2 ) }>+2</button>
+                            }
+                            <span>{ count }</span>                    
+                        </>
+                    )
                 }
+            </ProductCard>
 
-            </div>
 
-            <div className="shopping-cart">
-                {
-                    Object.entries( shoppingCart ).map( ([ key, product ]) =>(
-
-                        <ProductCard 
-                            key={ key }
-                            product={ product } 
-                            className="bg-dark text-white"
-                            style={{ width: '100px' }}
-                            onChange = { onProductCountChange }
-                            value={ product.count }
-                        >
-                            <ProductImage className="custom-image" />
-                            <ProductButtons 
-                                className="custom-buttons"
-                                style={{
-                                    display: 'flex',
-                                    justifyContent: 'center'
-                                }}
-                            />
-                        </ProductCard>
-
-                    ))
-                }
-            </div>
         </div>
     )
 }
